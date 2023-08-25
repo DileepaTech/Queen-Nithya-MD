47af7ef471c04171a4b5f3496f2696aee5a30346
Dileepa_Tech <103705712+DileepaTech@users.noreply.github.com>
2023-08-25 10:04:50 +0530

    NITHYA

--- commands/fullpp.js
+++ commands/fullpp.js
@@ -3,7 +3,7 @@
  Licensed under the  GPL-3.0 License;
  You may not use this file except in compliance with the License.
  It is supplied in the hope that it may be useful.
- * @project_name : Secktor-Md
+ * @project_name : Secktor-M
  * @author : @5hefin 
  * @description : Secktor,A Multi-functional whatsapp bot.
  * @version 0.0.6
