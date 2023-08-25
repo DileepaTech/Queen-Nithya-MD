01fd311781f4be48f98a4a7b2daef185e6859d61
Dileepa_Tech <103705712+DileepaTech@users.noreply.github.com>
2023-08-25 15:32:02 +0530

    Update scheduler.js

--- commands/scheduler.js
+++ commands/scheduler.js
@@ -1,13 +1,3 @@
-/**
- Copyright (C) 2022.
- Licensed under the  GPL-3.0 License;
- You may not use this file except in compliance with the License.
- It is supplied in the hope that it may be useful.
- * @project_name : Secktor-Md
- * @author : SamPandey001 <https://github.com/SamPandey001>
- * @description : Secktor,A Multi-functional whatsapp bot.
- * @version 0.0.6
- **/
 
  const { tlang,sck,prefix,cmd } = require('../lib')
  cmd({
